4d432a93d859ba3df26c9df8653f5e5809b2ecc0
jvnlee <lhjun1028@naver.com>
2023-02-11 20:09:33 +0900

    feat: 신규 value type인 Address를 추가하고, Member와 Delivery 엔티티에 적용

--- src/main/java/jpabasic/jpa/domain/Member.java
+++ src/main/java/jpabasic/jpa/domain/Member.java
@@ -11,11 +11,8 @@ public class Member extends BaseEntity {
 
     private String name;
 
-    private String city;
-
-    private String street;
-
-    private String zipcode;
+    @Embedded
+    private Address address;
 
     public Member() {
     }
@@ -36,28 +33,11 @@ public class Member extends BaseEntity {
         this.name = name;
     }
 
-    public String getCity() {
-        return city;
+    public Address getAddress() {
+        return address;
     }
 
-    public void setCity(String city) {
-        this.city = city;
+    public void setAddress(Address address) {
+        this.address = address;
     }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
-    }
-
 }
